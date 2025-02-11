46026befb179e69092fc878d8b5c3d27c9b1237e
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-02-11 08:41:31 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ import { Clock } from "lucide-react"
 
 export default function App() {
   return (
-    <div className="bg-neutral-100 dark:bg-neutral-900">
+    <div className="bg-neutral-100 dark:bg-neutral-900 w-full h-full">
       <Header/>
       <Notice/>
-      <main className="m-4">
-        <div className="flex flex-col md:flex-row gap-4">
-          <Card className="md:w-3/4 w-full">
+      <main className="w-full">
+        <div className="flex flex-col md:flex-row gap-4 w-full">
+          <Card className="w-full mt-2 md:w-3/4 md:mt-4 md:ml-4">
             <CardContent>
               <div className="mt-5">
                 <h1 className="text-3xl mb-3 font-bold">
@@ -36,24 +36,34 @@ export default function App() {
               최근변경일
             </CardFooter>
           </Card>
-          <Card className="md:w-1/4 w-500">
+          <Card className="w-full md:w-1/4 md:mt-4 md:mr-4">
             <CardHeader>
               <CardTitle className="flex gap-3 text-xl"><Clock />최근변경</CardTitle>
             </CardHeader>
+            <Separator/>
             <CardContent>
               <div x-show="$store.state.recent.length === 0">
                 문서를 편집해보세요!
               </div>
               <div x-show="$store.state.recent.length > 0">
-                문서 리스트
+                <ul>
+                  {window.store.recent.slice(0, 15).map((r, i) => (
+                    <li key={i}>
+                      <a href={`/w/${r.document}`} key={r}>{r.document}</a>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </CardContent>
           </Card>
         </div>
       </main>
-      <hr />
-      <footer className="flex bg-white dark:bg-black">
-        <a href="//testwiki.hyonsu.com">the tree</a>&nbsp;|&nbsp;<p>modern</p>
+      <footer>
+      <Card className="mt-4">
+        <CardHeader>
+          <CardTitle className="flex"><a href="//testwiki.hyonsu.com">the tree</a>&nbsp;|&nbsp;<p>modern</p></CardTitle>
+        </CardHeader>
+      </Card>
       </footer>
     </div>
   )
